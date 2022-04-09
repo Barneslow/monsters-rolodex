c063e81d862dbb8066a3064053507970f111ee5a
Barneslow <darrachb1991@gmail.com>
2022-04-09 12:28:09 +0100

    third commit

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,12 @@
-import { Component } from "react";
+import "./card-list.styles.css";
+import Card from "../card/card.component";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-
-    console.log("render");
-    return (
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
